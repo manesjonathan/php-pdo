a95e664c7a51ab17f750d086bd6fac3f69dec829
Jonathan Manes <manesjona@gmail.com>
2023-01-25 18:45:30 +0100

    sanitization and validation

--- php-training-mysql/public/hiking/create.php
+++ php-training-mysql/public/hiking/create.php
@@ -2,36 +2,62 @@
 
 use model\Hiking;
 
-session_start();
 include_once '../../database/HikingService.php';
 include_once '../../database/model/Hiking.php';
+session_start();
 
-if (isset($_POST['name']) && isset($_POST['difficulty']) && isset($_POST['distance']) && isset($_POST['duration']) && isset($_POST['height_difference'])) {
-    $name = $_POST['name'];
-    $difficulty = $_POST['difficulty'];
-    $distance = $_POST['distance'];
-    $duration = $_POST['duration'];
-    $available = $_POST['available'];
-    $height_difference = $_POST['height_difference'];
+$error_message = "";
 
-    $hiking = new Hiking();
-    $hiking->setName($name);
-    $hiking->setDifficulty($difficulty);
-    $hiking->setDistance($distance);
-    $hiking->setDuration($duration);
-    if ($available == "true") {
-        $availability_bool = true;
-    } else {
-        $availability_bool = false;
+if (isset($_POST['name']) && isset($_POST['difficulty']) && isset($_POST['distance']) && isset($_POST['duration']) && isset($_POST['height_difference']) && isset($_POST['available'])) {
+
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_SPECIAL_CHARS);
+    if (!$name || strlen($name) > 400) {
+        $error_message .= "Invalid name. ";
     }
-    $hiking->setAvailability($availability_bool);
-    $hiking->setHeightDifference($height_difference);
-    $hikingService = new HikingService();
-    $hikingService->createHiking($hiking);
 
-    $_SESSION['message'] = "Hiking added successfully.";
+    $distance = filter_var($_POST["distance"], FILTER_SANITIZE_NUMBER_FLOAT);
+    if (!$distance || $distance <= 0) {
+        $error_message .= "Invalid distance. ";
+    }
 
-    header('Location: read.php');
+    $height_difference = filter_var($_POST["height_difference"], FILTER_SANITIZE_NUMBER_INT);
+    if (!$height_difference || $height_difference <= 0) {
+        $error_message .= "Invalid height difference. ";
+    }
+
+    $duration = $_POST['duration'];
+    if (!preg_match('/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/', $duration)) {
+        $error_message .= "Invalid duration. ";
+    }
+
+    $difficulty = $_POST['difficulty'];
+    if (!in_array($difficulty, ["très facile", "facile", "moyen", "difficile", "très difficile"])) {
+        $error_message .= "Invalid difficulty. ";
+    }
+
+    $available = $_POST['available'];
+    if (!in_array($available, ["true", "false"])) {
+        $error_message .= "Invalid availability. ";
+    }
+
+    if (empty($error_message)) {
+        $hiking = new Hiking();
+        $hiking->setName($name);
+        $hiking->setDifficulty($difficulty);
+        $hiking->setDistance($distance);
+        $hiking->setDuration($duration);
+        $hiking->setAvailability($available == "true");
+        $hiking->setHeightDifference($height_difference);
+
+        $hikingService = new HikingService();
+        $hikingService->createHiking($hiking);
+
+        header('Location: read.php');
+    } else {
+        echo $error_message . '<br>';
+    }
+} else {
+    echo 'Fill all required fields <br>';
 }
 
 ?>
